621aa401a5770ab7596fca06d6f266a9e069e1ab
MHS195 <79396663+MHS195@users.noreply.github.com>
2025-06-13 08:57:30 +0300

    Update lineage_oce.mk

--- lineage_oce.mk
+++ lineage_oce.mk
@@ -23,7 +23,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/halium.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n.mk)
 
 # Inherit some common RR stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+#$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Include Maru stuff
 #$(call inherit-product, vendor/maruos/device-maru.mk)
